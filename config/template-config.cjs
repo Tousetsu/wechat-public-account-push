70ffed5a7584ad0fedff5d3b075241dca0fe2006
Tousetsu <111327312+Tousetsu@users.noreply.github.com>
2023-01-28 01:13:41 +0800

    some changes

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -52,6 +52,23 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+  {
+    id: '0003',
+    title: 'FCジョジ倶楽部 UPDATE',
+    desc: `
+      服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
+      
+      ---
+      
+      共推送 {{need_post_num.DATA}} 人
+      
+      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
+      
+      成功用户: {{success_post_ids.DATA}}
+      
+      失败用户: {{fail_post_ids.DATA}}
+    `
+  },
 ]
 
 module.exports = TEMPLATE_CONFIG
